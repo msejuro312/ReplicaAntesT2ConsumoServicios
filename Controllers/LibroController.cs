3c4ea42b31fa60ea584cc434aaff98aacc407293
msejuro312 <msejuro312@protonmail.com>
2026-08-08 20:47:43 -0500

    clase 08.08 modifico consumoservicios

---
+++ Controllers/LibroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LibroController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string apiBase = "https://localhost:7229";
+        public LibroController(HttpClient httpClient
+            ) {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(apiBase);
+
+
+        }
+
+        async List<Libro> listLibros()
+        { 
+            var response = await _httpClient.GetAsync ("api/libro");
+            var content = await response.Content.ReadAsStringAsync();
+            var lista = JsonConvert.DeserializeObject<List<Libro>>(content);
+        }
+        
+    }
+}
